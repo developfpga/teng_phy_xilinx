60a392c90e1ccc7f9c4be2cfcebe542c8cf48fef
m_sxl <xianglong.sun@magewell.com>
2018-08-24 17:47:01 +0800

    add scramble and descramble io define #1

---
+++ rtl/scramble.v
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// date : 2018-8-24
+///////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns/100ps
+
+module scramble{
+    input           clk_i,          // Freq = 156.25*2
+    input           rst_n_i,
+
+    input   [65:0]  data_i,         // [65:2] data, [1:0] head
+    input           data_vld_i,     // only valid data needs scramble
+    output  [65:0]  data_o,
+    output          data_vld_o      //
+
+};
+
+endmodule
